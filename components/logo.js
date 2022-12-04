d9ad3a4d9469a0a57f1e6ceb92c5b8957a938431
suntoes <mar.suntoes@gmail.com>
2022-12-04 21:58:41 +0800

    under development label

--- components/logo.js
+++ components/logo.js
@@ -1,10 +1,23 @@
 import Link from 'next/link'
-import { Image } from '@chakra-ui/react'
+import { Image, Text } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
 const Logo = props => (
   <Link href="/" scroll={false}>
     <a>
       <Image boxSize={50} src={'images/logo.png'} {...props} />
+      <Text
+        position="absolute"
+        color={{
+          base: 'red',
+          sm: 'orange',
+          md: 'yellow',
+          lg: 'green',
+          xl: 'blue'
+        }}
+      >
+        under development
+      </Text>
     </a>
   </Link>
 )
